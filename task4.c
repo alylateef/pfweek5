379f0cbc8958af1e8977b0e36d02cd4296fd3a93
Mohammad Ali <K250850@nu.edu.pk>
2025-09-15 10:25:27 +0500

    Add files via upload

---
+++ task4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main() {
+	float r;
+	printf("enter the radius here :\n");
+	scanf("%f",&r);
+	printf("area : %.2f \n circumference : %.2f \n square root of the radius : %.2f \n",3.142*r*r,2*3.142*r,sqrt(r));
+	
+	return 0;
+}
